e280bd104524e8616b0cb8a4b171f5545c029500
Dimifabbridf96 <dimi.fabbri@gmail.com>
2022-06-01 23:25:58 +0000

    little tests

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,40 +12,49 @@ let scissor = document.getElementById('scissor');
 let paper = document.getElementById('paper');
 let lizard = document.getElementById('lizard');
 let spock = document.getElementById('spock');
-
-
-function game(user){
-    function computerChoice(){
+  
+function computerChoice(){
     let choice = ['rock','scissor', 'paper', 'lizard', 'spock'];
     let randomChoice = Math.floor(Math.random() * 5);
     return choice[randomChoice];
-
-    }
-
-    if(user === 'rock' && computerChoice() === 'scissor' || 'lizard'){
+game();
+function game(user){ 
+  function checkWinner(){
+   
+        if(user === 'rock' && computerChoice() === 'scissor'){
 console.log(' user win');
 
-    } if(user === 'rock'  && computerChoice() === 'paper'|| 'spock'  ){
-        console.log('user lost');
-    }
-
- if(user === 'scissor' && computerChoice() === 'paper' || 'lizard'){
+    } if(user === 'rock'  && computerChoice === 'lizard'  ){
+        console.log('user win');
+    } if(user === 'rock'  && computerChoice === 'paper'  )
+        console.log('computer win');}
+     if(user === 'rock'  && computerChoice === 'spock'  ){
+            console.log('computer win');}
+        
+/**  if(user === 'scissor' && computerChoice() === 'paper' || 'lizard'){
         console.log('user win')
     } else if (computerChoice() === 'spock' || 'rock'){
         console.log( 'user lost');
+
            
         
-    }
+    
 
     if (user === 'paper'){
         if(computerChoice() === 'rock' || 'spock'){
             console.log('user win')
         }
     } 
-console.log(user);
-console.log(computerChoice());
+*/
 }
- game();
+    console.log(user);
+console.log(computerChoice());
+
+
+
+
+
+
 
 
 function click(){
@@ -65,8 +74,8 @@ lizard.addEventListener('click', function(){
 spock.addEventListener('click', function(){
     game('spock')
 })
-}
-click();
+} }
+
  
     
 
