b5b69aa5af4ca698c3dd0c66bb62051aeb8f5652
Dimifabbridf96 <dimi.fabbri@gmail.com>
2022-06-03 14:25:35 +0000

    start to put something together

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,62 +6,55 @@ let score = document.getElementById('scoreboard');
 let uScore = document.getElementsByClassName('u-score');
 let cScore = document.getElementsByClassName('c-score');
 let result = document.getElementsByClassName('result');
-let buttons = document.querySelectorAll('.choice');
+
 let rock = document.getElementById('rock');
 let scissor = document.getElementById('scissor');
 let paper = document.getElementById('paper');
 let lizard = document.getElementById('lizard');
 let spock = document.getElementById('spock');
-  
-function computerChoice(){
+
+document.addEventListener("DOMContentLoaded", game());
+
+function game(){
+ let buttons = document.getElementsByTagName('button');
+ let userChoice;
+ let computer;
+ for(let button of buttons){
+     button.addEventListener('click', function(){
+         if(this.getAttribute('data-type') === "rock"){
+             userChoice = this.getAttribute('data-type');
+             computer = computerChoice();
+         }
+               else if(this.getAttribute('data-type') === "paper"){
+                    userChoice = this.getAttribute('data-type');
+                    computer = computerChoice();
+        }
+                else if(this.getAttribute('data-type') === "scissor"){
+                        userChoice = this.getAttribute('data-type');
+                        computer = computerChoice();
+                }
+                    else if(this.getAttribute('data-type') === "lizard"){
+                            userChoice = this.getAttribute('data-type');
+                            computer = computerChoice();
+                        }
+                           else if(this.getAttribute('data-type') === "spock"){
+                                userChoice = this.getAttribute('data-type');
+                                computer = computerChoice();
+                            }
+              console.log(userChoice);
+              console.log(computer);
+                     
+              function computerChoice(){
     let choice = ['rock','scissor', 'paper', 'lizard', 'spock'];
     let randomChoice = Math.floor(Math.random() * 5);
-    return choice[randomChoice];
-game();
-function game(user){ 
-  function checkWinner(){
-   
-        if(user === 'rock' && computerChoice() === 'scissor'){
-console.log(' user win');
-
-    } if(user === 'rock'  && computerChoice === 'lizard'  ){
-        console.log('user win');
-    } if(user === 'rock'  && computerChoice === 'paper'  )
-        console.log('computer win');}
-     if(user === 'rock'  && computerChoice === 'spock'  ){
-            console.log('computer win');}
-        
-/**  if(user === 'scissor' && computerChoice() === 'paper' || 'lizard'){
-        console.log('user win')
-    } else if (computerChoice() === 'spock' || 'rock'){
-        console.log( 'user lost');
-
-           
-        
-    
-
-    if (user === 'paper'){
-        if(computerChoice() === 'rock' || 'spock'){
-            console.log('user win')
+    return choice[randomChoice]};
         }
-    } 
-*/
-}
-    console.log(user);
-console.log(computerChoice());
-
-
-
-
-
-
-
+ )};
 
 function click(){
 rock.addEventListener('click', function(){
     game('rock');
 })
-
 paper.addEventListener('click', function(){
     game('paper')
 })
@@ -74,8 +67,35 @@ lizard.addEventListener('click', function(){
 spock.addEventListener('click', function(){
     game('spock')
 })
-} }
+}} ;   
 
- 
+
+  
+/*
+function game(user){ }
+  function checkWinner(){
+   
+        if(user === 'rock' && computerChoice() === 'scissor'){
+console.log(' user win');
+
+    } if(user === 'rock'  && computerChoice === 'lizard'  ){
+        console.log('user win');
+    } if(user === 'rock'  && computerChoice === 'paper'  )
+        console.log('computer win');}
+     if(user === 'rock'  && computerChoice === 'spock'  ){
+            console.log('computer win');}
+        
+  if(user === 'scissor' && computerChoice() === 'paper' || 'lizard'){
+        console.log('user win')
+    } else if (computerChoice() === 'spock' || 'rock'){
+        console.log( 'user lost');
+    }
+           
+        
     
 
+    if (user === 'paper'){
+        if(computerChoice() === 'rock' || 'spock'){
+            console.log('user win')
+        }
+    } */
